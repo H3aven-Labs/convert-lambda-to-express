c04d7c76d97f4bbe0d4f960397b3fcb48aef4127
João Henrique Costa <joaoh9@pm.me>
2024-10-04 08:11:57 -0300

    fix: test

--- src/convertResponse.spec.ts
+++ src/convertResponse.spec.ts
@@ -152,7 +152,7 @@ describe('convertResponse()', () => {
     it('should return a valid response', () => {
       convertResponse(undefined, defaultResponse);
       expect(res.header).toHaveBeenNthCalledWith(1, 'content-type', defaultContentType);
-      expect(logger.info).toBeCalledTimes(2);
+      expect(logger.info).toBeCalledTimes(1);
       expect(logger.info).toHaveBeenNthCalledWith(1, 'End - Result:');
       expect(logger.info).toHaveBeenNthCalledWith(2, defaultResponse.body);
       expect(res.send).toBeCalledTimes(1);
