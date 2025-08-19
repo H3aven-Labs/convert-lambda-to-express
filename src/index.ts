ff1e7512127c8a672a18b0fee53f798f481ff947
João Henrique Costa <28352619+joaoh9@users.noreply.github.com>
2025-08-19 14:04:07 -0300

    faet: wrapLambdaWithAuthorizer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { EventOptions } from './Event';
 export { ContextOptions } from './Context';
 export { WrapperOptions, wrapLambda } from './wrapLambda';
+export { WrapperOptions as WrapperOptionsWithAuthorizer, wrapLambdaWithAuthorizer } from './wrapLambdaWithAuthorizer';
 export {
   addToDevServer,
   getDevServer,
