8943c9c812245f10e00c7c115cf5468356f4246e
João Henrique Costa <28352619+joaoh9@users.noreply.github.com>
2026-01-29 11:05:17 -0300

    feat: simplify build & rm unused dependencies

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,34 +1,19 @@
-import { build } from 'esbuild';
+import { build } from "esbuild";
 
-const isDarwin = process.platform === 'darwin';
-
-const externals = [
-  '@aws-sdk/credential-providers',
-  'chokidar',
-  'cors',
-  'express',
-  'helmet',
-  'jsonwebtoken',
-  'morgan',
-  'nodemon',
-];
-
-if (!isDarwin) {
-  externals.push('fsevents', 'async_hooks');
-}
+const externals = ["@aws-sdk/credential-providers", "express"];
 
 await build({
-  entryPoints: ['./src/index.ts'],
+  entryPoints: ["./src/index.ts"],
   bundle: true,
-  platform: 'node',
-  target: 'node18',
-  format: 'cjs',
-  outfile: './dist/index.js',
+  platform: "node",
+  target: "node18",
+  format: "cjs",
+  outfile: "./dist/index.js",
   minify: true,
-  drop: ['console'],
+  drop: ["console"],
   external: externals,
   define: {
-    'process.env.NODE_ENV': '"production"',
+    "process.env.NODE_ENV": '"production"',
   },
-  tsconfig: './tsconfig.json',
+  tsconfig: "./tsconfig.json",
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export { ContextOptions } from './Context';
-export { EventOptions } from './Event';
-export { wrapLambda, WrapperOptions } from './wrapLambda';
-export {
-  wrapLambdaWithAuthorizer,
-  WrapperOptions as WrapperOptionsWithAuthorizer,
-} from './wrapLambdaWithAuthorizer';
+export { ContextOptions } from "./Context";
+export { EventOptions } from "./Event";
+export { wrapLambda, WrapperOptions } from "./wrapLambda";
